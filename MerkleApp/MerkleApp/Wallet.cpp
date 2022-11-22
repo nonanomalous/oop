dd193109eb2220752780c663951904700d465737
Brett Monson <brett.monson@gonavis.com>
2022-11-22 11:01:45 -0700

    week 9b

--- MerkleApp/MerkleApp/Wallet.cpp
+++ MerkleApp/MerkleApp/Wallet.cpp
@@ -4,11 +4,26 @@ Wallet::Wallet()
 {}
 
 void Wallet::insertCurrency(std::string type, double amount)
-{}
+{
+	if (amount < 0) 
+		throw std::exception{ "Cannot insert negative amounts" };
+	
+	double balance;
+	
+	if (currencies.count(type) == 0)
+		balance = 0;
+	else
+		balance = currencies[type];
+
+	currencies[type] = balance + amount;
+}
 
 bool Wallet::containsCurrency(std::string type, double amount)
 {
-	return false;
+	if (currencies.count(type) == 0)
+		return false;
+	else
+		return currencies[type] >= amount;
 }
 
 std::string Wallet::toString()
--- MerkleApp/MerkleApp/main.cpp
+++ MerkleApp/MerkleApp/main.cpp
@@ -5,10 +5,15 @@
 #include "OrderBookEntry.h"
 #include "MerkleMain.h"
 #include "CSVReader.h"
+#include "Wallet.h"
 
 int main()
 {
-    MerkleMain app{};
-    app.init();
+    //MerkleMain app{};
+    //app.init();
+    Wallet wallet;
+    wallet.insertCurrency("BTC",5);
+    std::cout << "Wallet has BTC: " << wallet.containsCurrency("BTC", 10) << std::endl;
+    std::cout << wallet.toString() << std::endl;
     return 0;
 }
